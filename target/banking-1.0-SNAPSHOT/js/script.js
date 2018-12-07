369f0d82eef6951cc4be6c063acc2912dcaea06e
Dan Nemtanu <dannemtanu@Dans-MacBook-Pro.local>
2018-12-07 13:59:00 +0000

    JS actions

--- target/banking-1.0-SNAPSHOT/js/script.js
+++ target/banking-1.0-SNAPSHOT/js/script.js
@@ -3,20 +3,22 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-alert("JS");
+
+var customerID;
+
 function registerAccount() {
     var fname = $("#full-name").val();
     var address = $("#add1").val() + ", " + $("#add2").val() + ", " + $("#add3").val();
     var eamil = $("#email").val();
     var pass = $("#pass").val();
     var cpass = $("#c_password").val();
-    
-    var url = "/api/customer/create?name="+fname+"&address="+address+"&email="+email+"&password="+pass;
+
+    var url = "/api/customer/create?name=" + fname + "&address=" + address + "&email=" + email + "&password=" + pass;
     $.post(url, function (data) {
         console.log(data);
         alert("Account Created");
     });
-    
+
 }
 $("#formsubmit").click(function () {
     registerAccount();
@@ -25,3 +27,14 @@ $("#formsubmit").click(function () {
         alert("Load was performed.");
     });
 });
+
+$("#login").click(function (e) {
+    e.preventDefault();
+    customerID = $("#customerID").val();
+    var pass = $("#customerPassword").val();
+    $(location).attr('href', 'http://localhost:49000/customer_main.html');
+});
+
+// Displaying Details 
+console.log("Customer ID: " + customerID);
+$("#globalID").innerHTML = customerID;
